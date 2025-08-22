f0981690f2fb87ca6a0623ee13bd07f110c1a0c3
Pramod D K <pramoddk8590@gmail.com>
2025-08-22 11:56:30 +0530

    @RequestBody added

--- first-api-learning/src/main/java/com/acciojob/first_api_learning/apiClass.java
+++ first-api-learning/src/main/java/com/acciojob/first_api_learning/apiClass.java
@@ -1,14 +1,49 @@
 package com.acciojob.first_api_learning;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
-@RequestMapping("v2")
+//@RequestMapping("v2")  //used to add some prefix to the url
 public class apiClass {
+
+    Map<Integer,Patient> patientDB = new HashMap<>();
+
+    //Using @RequestParam
+    @PostMapping("/addpatientdetails")
+    public String addpatientinfo(@RequestParam("id") int id,
+                                 @RequestParam("name") String name,
+                                 @RequestParam("age") int age,
+                                 @RequestParam("disease") String disease){
+        Patient p= new Patient(id,name,age,disease);
+        int key= id;
+        patientDB.put(id,p);
+        return "Patient info added";
+    }
+    @GetMapping("/getpatieninfo")
+    public Patient getPatienInfo(@RequestParam("id") Integer id){
+        Patient p1 = patientDB.get(id);
+        return p1;
+    }
+
+
+    //using @RequestBody
+    @PostMapping("/addpatientDBviaRequestBody")
+    public String addinfoviaRequestBody(@RequestBody Patient patient){
+        Integer key = patient.getId();
+        patientDB.put(key,patient);
+        return "patient added to db";
+    }
+    //not possible
+    @GetMapping("/getPatientinfoviaRequestBody")
+    public Patient getPatientviabody(@RequestBody Patient id){
+        Integer key =patientDB.get(id).getId();
+        Patient patient= patientDB.get(key);
+        return patient;
+    }
 
     @GetMapping("getWeatherInfo")
 
